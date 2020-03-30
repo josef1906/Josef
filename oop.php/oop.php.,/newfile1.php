bdc474a88c1e5427e083333ae2b439dd1d950954
josef1906 <62419301+josef1906@users.noreply.github.com>
2020-03-30 17:07:17 +0200

    Add files via upload

--- oop.php/oop.php.,/newfile1.php
+++ oop.php/oop.php.,/newfile1.php
@@ -8,3 +8,33 @@ abstract class ustrednaPrefix {
 var $napeti ;
 
 ?>
+<?php
+public function translate(string $text) : STRING {
+    
+    $translate_text = $THIS->TS->translate($text);
+    
+    RETURN $translate_text;
+}
+?>
+?>
+<?php
+public function translate(string $text) : string{
+    $translate_text = $this->ts->translate($text);
+    
+    return $translate_text;
+}
+?>
+<?php
+public function translate(string $text) : string {
+    
+    return $translate_text;
+}
+?>
+<?php
+public function translate(string $text){
+    
+    $translate_text = $this->ts->translate($text)
+    
+    return $translate_text;
+}
+?>
